97ca0dfea7eab151f9e885f73c30b0483cdacfd5
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2022-03-16 11:05:47 +0100

    y
    y

--- Gestion-Employes-brief/php/navbar.php
+++ Gestion-Employes-brief/php/navbar.php
@@ -1,27 +1,24 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-nav">
 
-    
-    <img src="/App_Gestion_Employees-/Gestion-Employes-brief/images/logo.png" alt="logo" width="100px">
-    <div class="collapse navbar-collapse" id="navbarText">
-      
-      <ul class="navbar-nav m-auto mb-2 mb-lg-0">
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Select a Page
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li>
-              <a class="nav-link active" aria-current="page" href="/App_Gestion_Employees-/Gestion-Employes-brief/view.php">
-                <p class="text-dark">Liste des employées</p>
-              </a>
-            </li>
-            <li>
-              <a class="nav-link" href="/App_Gestion_Employees-/Gestion-Employes-brief/index.php">
-                <p class="text-info">Ajouter un employé</p>
-              </a>
-            </li>
-          </ul>
 
-
+  <img src="/App_Gestion_Employees-/Gestion-Employes-brief/images/logo.png" alt="logo" width="100px">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+      <a class="nav-link active" aria-current="page" href="/App_Gestion_Employees-/Gestion-Employes-brief/view.php">
+              <p class="text-dark">Liste des employées</p>
+            </a>
+      </li>
+      <li class="nav-item">
+      <a class="nav-link" href="/App_Gestion_Employees-/Gestion-Employes-brief/index.php">
+              <p class="text-info">Ajouter un employé</p>
+            </a>
+      </li>
+      <li class="nav-item">
+      <a class="nav-link" href="/App_Gestion_Employees-/Gestion-Employes-brief/index.php">
+              <p class="text-info">Rechercher un Employé</p>
+            </a>
+      </li>
+    </ul>
   </div>
 </nav>
